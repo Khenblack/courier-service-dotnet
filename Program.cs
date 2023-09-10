647a7f233874d8797467843ed8789a89189aa5c8
Ernest Roca <e.roca@stuart.com>
2023-09-10 17:03:22 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using CoutierServiceDotnet.Swagger;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddApiVersioning(opt =>
+        {
+            opt.DefaultApiVersion = new ApiVersion(1, 0);
+            opt.AssumeDefaultVersionWhenUnspecified = true;
+            opt.ReportApiVersions = true;
+            opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                            new HeaderApiVersionReader("x-api-version"),
+                                                            new MediaTypeApiVersionReader("x-api-version"));
+        });
+
+        builder.Services.AddVersionedApiExplorer(
+            options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        builder.Services.AddCustomSwagger();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseCustomSwagger();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
---
+++ Swagger/SwaggerExtensions.cs
@@ -0,0 +1,39 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace CoutierServiceDotnet.Swagger
+{
+    public static class SwaggerExtensions
+    {
+        public static void AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.OperationFilter<SwaggerDefaultValues>();
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+            services.ConfigureOptions<ConfigureSwaggerOptions>();
+        }
+        public static void UseCustomSwagger(this WebApplication app)
+        {
+            var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+            app.UseSwagger(options => options.RouteTemplate = "swagger/{documentName}/swagger.json");
+            app.UseSwaggerUI(options =>
+            {
+                var descriptions = provider.ApiVersionDescriptions;
+
+                // Build a swagger endpoint for each discovered API version
+                foreach (var description in descriptions)
+                {
+                    var url = $"/swagger/{description.GroupName}/swagger.json";
+                    var name = description.GroupName.ToUpperInvariant();
+                    options.SwaggerEndpoint(url, name);
+                }
+            });
+        }
+    }
+
+}
